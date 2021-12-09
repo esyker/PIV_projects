cf14c8f8c1d7a7aca133befa3e26afc836f3f1dd
Utilizador <ihaelm@gmail.com>
2021-12-09 15:04:47 +0000

    addded the dataset

---
+++ papertablet.m
@@ -0,0 +1,21 @@
+%%
+%Images to depth load
+depth2cams_0=load('Dataset/imgs2depth/depth2cams_0000.mat');
+depth2cams_1=load('Dataset/imgs2depth/depth2cams_0001.mat');
+depth2cams_2=load('Dataset/imgs2depth/depth2cams_0002.mat');
+depth2cams_3=load('Dataset/imgs2depth/depth2cams_0003.mat');
+depth2cams_4=load('Dataset/imgs2depth/depth2cams_0004.mat');
+depth2cams_5=load('Dataset/imgs2depth/depth2cams_0005.mat');
+
+%%
+%Just depth load
+depth_0=load('Dataset/teste/depth_0000.mat');
+depth_1=load('Dataset/teste/depth_0001.mat');
+depth_2=load('Dataset/teste/depth_0002.mat');
+depth_3=load('Dataset/teste/depth_0003.mat');
+depth_4=load('Dataset/teste/depth_0004.mat');
+depth_5=load('Dataset/teste/depth_0005.mat');
+
+%%
+%Calib Asus load
+calib_asus=load('Dataset/calib_asus.mat');
