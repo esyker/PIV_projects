85e360ab67cd470fa276bb523f81039e1764d569
Utilizador <ihaelm@gmail.com>
2021-12-15 12:39:20 +0000

    findHomography version by hand

--- papertablet.py
+++ papertablet.py
@@ -12,6 +12,23 @@ import scipy.io
 import time
 import matplotlib.pyplot as plt
 
+def findHomography(sourcePoints, destPoints):
+    A=[]
+    for i in range(4):
+        x1=sourcePoints[i][0]
+        y1=sourcePoints[i][1]
+        x2=destPoints[i][0]
+        y2=destPoints[i][1]
+        line1=[x1,y1,1,0,0,0,-x1*x2,-y1*x2,-x2]
+        line2=[0,0,0,x1,y1,1,-x1*y2,-y1*y2,-y2]
+        A.append(line1)
+        A.append(line2)
+    u, s, vh = np.linalg.svd(A,full_matrices=True)#svd decomposition
+    h=vh[-1]
+    h=h.reshape((3,3))
+    h=h/h[2][2]#normalize matrix
+    return h
+    
 def getSourceCorners(arucos):
     sourceCorners = []
     numb_arucos = len(arucos["corners"])
@@ -22,7 +39,12 @@ def getSourceCorners(arucos):
 
 def getDestCorners(sourceIDs,referenceCorners):
     destCorners=[]
+    numb_arucos=0
     for _id in sourceIDs:
+        if numb_arucos==4:
+            break
+        #print(_id)
+        numb_arucos+=1
         corner = referenceCorners[_id[0]]
         for point in corner: 
             destCorners.append(point)
@@ -55,9 +77,18 @@ while(cap.isOpened()):
         arucos=getArucos(frame)
         corners=getSourceCorners(arucos)
         destCorners= getDestCorners(arucos["ids"],referenceCorners)
-        M,mask = cv2.findHomography(corners, destCorners)
+        #M,mask = cv2.findHomography(corners, destCorners)#, method=cv2.RANSAC,ransacReprojThreshold=5.0)
+        M= findHomography(corners, destCorners)
         rotated=cv2.warpPerspective(frame,M,(frame.shape[1],frame.shape[0]))
         #print(M)
+        #
+        frame=cv2.circle(frame, (int(corners[0][0]),int(corners[0][1])), 
+                   radius=10, color=(0, 0, 255), thickness=10)
+        rotated=cv2.circle(rotated, (int(destCorners[0][0]),int(destCorners[0][1])), 
+                   radius=10, color=(255, 0, 0), thickness=10)
+        homography_point = np.matmul(M,np.array([corners[0][0],corners[0][1],1]))
+        rotated=cv2.circle(rotated, (int(homography_point[0]),int(homography_point[1])), 
+                   radius=10, color=(0, 255, 0), thickness=10)
         concatenated = np.concatenate((frame,rotated),axis=1)
         concatenated=cv2.resize(concatenated,(960,540))
         cv2.imshow("original and homography",concatenated)
@@ -73,12 +104,13 @@ print("--- %s seconds ---" % (time.time() - start_time))
 cap.release()
 cv2.destroyAllWindows()
 
+
 #check if homography is correct
 plt.figure()
 plt.imshow(frame)
-original_point=np.array([1254,302,1])
-plt.scatter(original_point[0], original_point[1], s=5, c='red', marker='o')
+original_point=np.array([1532,453,1])
+plt.scatter(original_point[0], original_point[1], s=3, c='red', marker='o')
 plt.figure()
 plt.imshow(rotated)
 rotated_point = np.matmul(M,original_point)
-plt.scatter(rotated_point[0], rotated_point[1], s=5, c='red', marker='o')
+plt.scatter(rotated_point[0], rotated_point[1], s=3, c='red', marker='o')
