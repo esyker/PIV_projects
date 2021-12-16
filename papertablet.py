325e57f0fdd12d8571d97fdca22f62f9e589d17d
Utilizador <ihaelm@gmail.com>
2021-12-16 13:52:01 +0000

    working

--- papertablet.py
+++ papertablet.py
@@ -14,7 +14,7 @@ import matplotlib.pyplot as plt
 
 def findHomography(sourcePoints, destPoints):
     A=[]
-    for i in range(4,8):
+    for i in range(16):
         x1=sourcePoints[i][0]
         y1=sourcePoints[i][1]
         x2=destPoints[i][0]
@@ -58,14 +58,20 @@ def getReferenceCorners(referenceArucos):
                                                           in referenceArucos["corners"][i][0]]
     return referenceCorners
 
+template = cv2.cvtColor(cv2.imread('Dataset/template2_fewArucos.png'), cv2.COLOR_BGR2GRAY)
+#template = cv2.imread('Dataset/template2_fewArucos.png')
 
+rect_template = np.array([[[0,0],
+                          [template.shape[1],0],
+                          [template.shape[1],template.shape[0]],
+                           [0,template.shape[0]]]],dtype="float32")
 #run('Dataset/template1_manyArucos.png');
 run('Dataset/template2_fewArucos.png')
 
 referenceArucos = scipy.io.loadmat('cornersIds.mat')
 referenceCorners=getReferenceCorners(referenceArucos)
 
-input_video_path = './Dataset/FewArucos-Viewpoint1.mp4'
+input_video_path = './Dataset/FewArucos-Viewpoint2.mp4'
 
 #Show video
 cap = cv2.VideoCapture(input_video_path)
@@ -74,24 +80,40 @@ start_time = time.time()
 while(cap.isOpened()):
     ret, frame = cap.read()
     if ret:
+        gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
         arucos=getArucos(frame)
-        corners=getSourceCorners(arucos)
-        destCorners= getDestCorners(arucos["ids"],referenceCorners)
-        #M,mask = cv2.findHomography(corners, destCorners)#, method=cv2.RANSAC,ransacReprojThreshold=5.0)
-        M= findHomography(corners, destCorners)
-        rotated=cv2.warpPerspective(frame,M,(frame.shape[1],frame.shape[0]))
+        if(len(arucos["ids"])>0):
+            corners=getSourceCorners(arucos)
+            destCorners= getDestCorners(arucos["ids"],referenceCorners)
+            print("Corners",corners)
+            print("DestCorners",destCorners)
+            #M,mask = cv2.findHomography(corners, destCorners)#, method=cv2.RANSAC,ransacReprojThreshold=5.0)
+            H= findHomography(destCorners, corners)
+            #M= findHomography(corners, destCorners)
+            #rotated=cv2.warpPerspective(frame,M,(frame.shape[1],frame.shape[0]))
+            #rotated = cv2.perspectiveTransform(frame,M,(frame.shape[1],frame.shape[0]))
+            rect_frame = cv2.perspectiveTransform(rect_template, H,(gray.shape[1],gray.shape[0]))
+            print("H",H)
+        M= cv2.getPerspectiveTransform(rect_frame,rect_template)
+        print("M",M)
+        rotated = cv2.warpPerspective(frame,M, (template.shape[1],template.shape[0]))
         #print(M)
         #
+        """
         frame=cv2.circle(frame, (int(corners[0][0]),int(corners[0][1])), 
-                   radius=10, color=(0, 0, 255), thickness=10)
+                   radius=10, color=(0, 0, 255), thickness=10)#red
         rotated=cv2.circle(rotated, (int(destCorners[0][0]),int(destCorners[0][1])), 
-                   radius=10, color=(255, 0, 0), thickness=10)
+                   radius=10, color=(255, 0, 0), thickness=10)#blue
         homography_point = np.matmul(M,np.array([corners[0][0],corners[0][1],1]))
         rotated=cv2.circle(rotated, (int(homography_point[0]),int(homography_point[1])), 
-                   radius=10, color=(0, 255, 0), thickness=10)
-        concatenated = np.concatenate((frame,rotated),axis=1)
-        concatenated=cv2.resize(concatenated,(960,540))
-        cv2.imshow("original and homography",concatenated)
+                   radius=10, color=(0, 255, 0), thickness=10)#green
+        """
+        print(rotated.shape)
+        #concatenated = np.concatenate((frame,rotated),axis=1)
+        #concatenated=cv2.resize(concatenated,(960,540))
+        #cv2.imshow("original and homography",concatenated)
+        im_out=cv2.resize(rotated,None,fx=0.2,fy=0.2)
+        cv2.imshow("original and homo",im_out)
         k=cv2.waitKey(30) & 0xff
         #once you inter video will stop
         if k==27:
