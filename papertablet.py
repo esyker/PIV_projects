0bb6bec1a82c7b4006b7fe6747d8bb05954b929f
Utilizador <ihaelm@gmail.com>
2022-01-04 20:14:40 +0000

    more clean code

--- papertablet.py
+++ papertablet.py
@@ -1,16 +1,36 @@
-#Some references
-#https://docs.google.com/document/d/e/2PACX-1vRF494drpSOiZ6In-KKg98eQ6iS4XdBwDFywW4j9SNZKmcJJ3-jRE-_86MVq6GUFg/pub
-#https://docs.opencv.org/4.x/d5/dae/tutorial_aruco_detection.html
-#https://docs.opencv.org/4.x/d5/dae/tutorial_aruco_detection.html#:~:text=10%20%2Did%3D23-,Marker%20Detection,-Given%20an%20image
-#https://www.pyimagesearch.com/2020/12/21/detecting-aruco-markers-with-opencv-and-python/
-#https://www.pyimagesearch.com/2018/07/23/simple-object-tracking-with-opencv/
 import cv2
 import numpy as np
-from getCorners import run 
-from getCorners import getArucos
-import scipy.io
 import time
-import matplotlib.pyplot as plt
+
+def initArucoPos(template, aruco_dict, arucoParameters):
+    """
+    Returns the corners of the Aruco markers in the template image. Note that the corners sequence is the same as
+    the ids sequence. Example: if ids[0] has value 2 then corners[0] has corners of Aruco marker with id=2.
+    :param template: template image
+    :param aruco_dict: dictionary of Aruco codes used in the template image
+    """
+    gray_template = cv2.cvtColor(template, cv2.COLOR_BGR2GRAY)
+    corners, ids, rejectedImgPoints = cv2.aruco.detectMarkers(gray_template, aruco_dict, parameters=arucoParameters)
+    if len(corners) == 0:
+        print("getCorners: Could not detect Aruco markers. Exiting.")
+        exit(0)
+    return corners, ids
+
+def getArucos(img):
+    """
+    This function receives an image as input and returns the Aruco markers' coordinates (in pixels)
+    and ids. The order of the coordinates is the same as the order of the ids. Example: if ids[0] has value 2 then
+    corners[0] has corners of Aruco marker with id=2.
+    """
+    if img is None:
+        print("getCorners: Unable to read the template.")
+        exit(-1)
+    #The arucos in the template must be the ones in this dictionary
+    dict4_7by7 = cv2.aruco.getPredefinedDictionary(cv2.aruco.DICT_7X7_50)
+    arucoParameters = cv2.aruco.DetectorParameters_create()
+    corners, ids = initArucoPos(img, dict4_7by7, arucoParameters)
+    dict = {"corners": corners, "ids": ids}
+    return dict
 
 def findHomography(sourcePoints, destPoints):
     A=[]
@@ -58,20 +78,17 @@ def getReferenceCorners(referenceArucos):
                                                           in referenceArucos["corners"][i][0]]
     return referenceCorners
 
-template = cv2.cvtColor(cv2.imread('Dataset/template2_fewArucos.png'), cv2.COLOR_BGR2GRAY)
-#template = cv2.imread('Dataset/template2_fewArucos.png')
+input_video_path = './Dataset/FewArucos-Viewpoint2.mp4'
+template_path = 'Dataset/template2_fewArucos.png'
+img_template = cv2.imread(template_path)
 
 rect_template = np.array([[[0,0],
-                          [template.shape[1],0],
-                          [template.shape[1],template.shape[0]],
-                           [0,template.shape[0]]]],dtype="float32")
-#run('Dataset/template1_manyArucos.png');
-run('Dataset/template2_fewArucos.png')
+                          [img_template.shape[1],0],
+                          [img_template.shape[1],img_template.shape[0]],
+                           [0,img_template.shape[0]]]],dtype="float32")
 
-referenceArucos = scipy.io.loadmat('cornersIds.mat')
+referenceArucos = getArucos(img_template)
 referenceCorners=getReferenceCorners(referenceArucos)
-
-input_video_path = './Dataset/FewArucos-Viewpoint2.mp4'
 
 #Show video
 cap = cv2.VideoCapture(input_video_path)
@@ -85,37 +102,18 @@ while(cap.isOpened()):
         if(len(arucos["ids"])>0):
             corners=getSourceCorners(arucos)
             destCorners= getDestCorners(arucos["ids"],referenceCorners)
-            print("Corners",corners)
-            print("DestCorners",destCorners)
-            #M,mask = cv2.findHomography(corners, destCorners)#, method=cv2.RANSAC,ransacReprojThreshold=5.0)
             H= findHomography(destCorners, corners)
-            #M= findHomography(corners, destCorners)
-            #rotated=cv2.warpPerspective(frame,M,(frame.shape[1],frame.shape[0]))
-            #rotated = cv2.perspectiveTransform(frame,M,(frame.shape[1],frame.shape[0]))
             rect_frame = cv2.perspectiveTransform(rect_template, H,(gray.shape[1],gray.shape[0]))
-            print("H",H)
         M= cv2.getPerspectiveTransform(rect_frame,rect_template)
-        print("M",M)
-        rotated = cv2.warpPerspective(frame,M, (template.shape[1],template.shape[0]))
-        #print(M)
-        #
-        """
-        frame=cv2.circle(frame, (int(corners[0][0]),int(corners[0][1])), 
-                   radius=10, color=(0, 0, 255), thickness=10)#red
-        rotated=cv2.circle(rotated, (int(destCorners[0][0]),int(destCorners[0][1])), 
-                   radius=10, color=(255, 0, 0), thickness=10)#blue
-        homography_point = np.matmul(M,np.array([corners[0][0],corners[0][1],1]))
-        rotated=cv2.circle(rotated, (int(homography_point[0]),int(homography_point[1])), 
-                   radius=10, color=(0, 255, 0), thickness=10)#green
-        """
-        print(rotated.shape)
-        #concatenated = np.concatenate((frame,rotated),axis=1)
-        #concatenated=cv2.resize(concatenated,(960,540))
-        #cv2.imshow("original and homography",concatenated)
-        im_out=cv2.resize(rotated,None,fx=0.2,fy=0.2)
-        cv2.imshow("original and homo",im_out)
-        k=cv2.waitKey(30) & 0xff
-        #once you inter video will stop
+        rotated = cv2.warpPerspective(frame,M, (img_template.shape[1],img_template.shape[0]))
+        #resize
+        frame = cv2.resize(frame,None,fx=0.2,fy=0.2)
+        rotated = cv2.resize(rotated,None,fx=0.2,fy=0.2)
+        cv2.imshow("original",frame)
+        cv2.imshow("homography",rotated)
+        
+        k=cv2.waitKey(1) & 0xff
+        #once enter key is pressed video will stop
         if k==27:
             break
     else:
@@ -125,14 +123,3 @@ print("--- %s seconds ---" % (time.time() - start_time))
 
 cap.release()
 cv2.destroyAllWindows()
-
-
-#check if homography is correct
-plt.figure()
-plt.imshow(frame)
-original_point=np.array([1532,453,1])
-plt.scatter(original_point[0], original_point[1], s=3, c='red', marker='o')
-plt.figure()
-plt.imshow(rotated)
-rotated_point = np.matmul(M,original_point)
-plt.scatter(rotated_point[0], rotated_point[1], s=3, c='red', marker='o')
